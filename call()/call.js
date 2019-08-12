f86db8a89a1d59b60a83ebc33f6128bb489e1bd8
bs185_000@laptop <bs1852985@gmail.com>
2019-08-12 13:39:40 +0530

    call function

--- call()/call.js
+++ call()/call.js
@@ -20,3 +20,15 @@ obj.fullName.call(obj2);
 //passing argumnets using call()
 obj.fullName.call(obj2,"mumbai");
 obj.fullName("jodhpur");
+
+//creating func outside the obj
+let name={
+    first:"harshit",
+    last:"jangid",
+    
+}
+
+let fullName=function(state){
+    console.log(this.first+" "+this.last+" "+state);
+}
+fullName.call(name,"bihar");
