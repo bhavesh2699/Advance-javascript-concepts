fc53883953f7832cae3f621e496838cc652e6dd9
bs185_000@laptop <bs1852985@gmail.com>
2019-08-12 13:33:00 +0530

    call function

--- call()/call.js
+++ call()/call.js
@@ -2,16 +2,21 @@
 let obj={
     name:"bhavesh",
     last:"solanki",
-    fullName:function(){
-        console.log(this.name+" "+this.last);
+    fullName:function(state){
+        console.log(this.name+" "+this.last+" "+state);
     }
 }
 obj.fullName();
 
 //function call using call() function
+//creating obj2 and reusing fullname function of obj
 let obj2={
     name:"salman",
     last:"khan",
 }
 
 obj.fullName.call(obj2);
+
+//passing argumnets using call()
+obj.fullName.call(obj2,"mumbai");
+obj.fullName("jodhpur");
